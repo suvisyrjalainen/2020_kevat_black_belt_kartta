2b1d69e607e8c6f483f6060e936d7f2bcecaed06
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2020-11-11 17:12:36 +0200

    kevään muutokset

--- js/kartat.js
+++ js/kartat.js
@@ -1,3 +1,7 @@
+var map;
+var latitude;
+var longitude;
+
 function valmista(){
 
   if("geolocation" in navigator) {
@@ -5,12 +9,12 @@ function valmista(){
   navigator.geolocation.getCurrentPosition(function(position) {
       console.log(position);
 
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
 
 
 
-    var map = L.map('kartta').setView([latitude, longitude], 13);
+    map = L.map('kartta').setView([latitude, longitude], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -28,5 +32,17 @@ function valmista(){
 function tallenna_sijainti() {
   var sijaintilomake = document.getElementById("arvostelulomake");
   sijaintilomake.style.display = "block";
+
+}
+
+function kirjoita_kartalle(){
+  var paikka = document.getElementById("paikka").value;
+  var arvostelu = document.getElementById("arvostelu").value;
+  console.log(paikka);
+  console.log(arvostelu);
+
+  L.marker([latitude, longitude]).addTo(map)
+  .bindPopup(paikka + "<br>" + arvostelu)
+  .openPopup();
 
 }
